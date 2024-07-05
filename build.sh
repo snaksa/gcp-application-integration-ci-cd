a4bb6c3144c13b6373511b49dd9d6e344db51a04
sinan-shaban-freestar <sinan.shaban@freestar.io>
2024-07-05 18:04:32 +0300

    use sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo "Token: $token"
 echo "Current directory: $(pwd)"
 ls -la
 docker run us-docker.pkg.dev/appintegration-toolkit/images/integrationcli integrations list -p "$project" -r "$region" -t "$token"
-docker run -v "/workspace:/workspace" us-docker.pkg.dev/appintegration-toolkit/images/integrationcli integrations apply -f . -e dev --wait=true -p "$project" -r "$region" -t "$token"
+docker run -v "/workspace:/workspace" us-docker.pkg.dev/appintegration-toolkit/images/integrationcli integrations apply -f /workspace -e dev --wait=true -p "$project" -r "$region" -t "$token"
 
 #docker run \
 #          -v "$(pwd):/workspace" \
