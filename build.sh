8f195172a3834d3cc0d2691e385461d123104566
sinan-shaban-freestar <sinan.shaban@freestar.io>
2024-07-05 15:54:43 +0300

    use sh

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@
 token=$(gcloud auth print-access-token)
 project=$(gcloud config get-value project | head -n 1)
 region=us-central1
+echo "Project: $project"
+echo "Region: $region"
+echo "Token: $token"
 docker run us-docker.pkg.dev/appintegration-toolkit/images/integrationcli integrations list -p "$project" -r "$region" -t "$token"
