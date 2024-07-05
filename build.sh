1840899487ae4a7cd02b6af61cf0d18a8a1b4691
sinan-shaban-freestar <sinan.shaban@freestar.io>
2024-07-05 16:37:11 +0300

    use sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ region=us-central1
 echo "Project: $project"
 echo "Region: $region"
 echo "Token: $token"
-docker run -v us-docker.pkg.dev/appintegration-toolkit/images/integrationcli integrations list -p "$project" -r "$region" -t "$token"
-docker run -v us-docker.pkg.dev/appintegration-toolkit/images/integrationcli integrations apply -f . -e dev --wait=true -p "$project" -r "$region" -t "$token"
+docker run us-docker.pkg.dev/appintegration-toolkit/images/integrationcli integrations list -p "$project" -r "$region" -t "$token"
+docker run us-docker.pkg.dev/appintegration-toolkit/images/integrationcli integrations apply -f . -e dev --wait=true -p "$project" -r "$region" -t "$token"
