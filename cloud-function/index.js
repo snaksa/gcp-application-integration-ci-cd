7a4dea38e836f06c15a1008c61e9145df69ed478
sinan-shaban-freestar <sinan.shaban@freestar.io>
2024-07-09 18:42:56 +0300

    Add cloud function deployment

--- cloud-function/index.js
+++ cloud-function/index.js
@@ -1,6 +1,6 @@
 const functions = require('@google-cloud/functions-framework');
 
-functions.http('helloHttp', async (req, res) => {
+functions.http('poc-function-deployment', async (req, res) => {
     console.log('Hello, world!!!');
 
     res.status(200).send({success: true});
