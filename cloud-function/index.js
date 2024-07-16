4b0c57424a7fc70dc09ccbff0aade8a69bd2220c
sinan-shaban-freestar <sinan.shaban@freestar.io>
2024-07-16 12:07:57 +0300

    Add env vars

--- cloud-function/index.js
+++ cloud-function/index.js
@@ -2,13 +2,8 @@ const functions = require('@google-cloud/functions-framework');
 
 functions.http('poc-function-deployment', async (req, res) => {
     console.log('Hello, world!!!');
-    console.log({
-        MY_SECRET: process.env.MY_SECRET,
-        MY_ULTRA_SECRET: process.env.MY_ULTRA_SECRET
-    })
 
     res.status(200).send({
         MY_SECRET: process.env.MY_SECRET,
-        MY_ULTRA_SECRET: process.env.MY_ULTRA_SECRET
     });
 });
